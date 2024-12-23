e5e3b5a8d08e689458efa07e08b7e32105f04bb2
Pavlo Berezin <160422966+NureBerezinPavlo@users.noreply.github.com>
2024-12-23 21:01:11 +0100

    Delete Task2/project directory

--- Task2/project/app/models/elevator.py
+++
@@ -1,9 +0,0 @@
-from app import db
-
-class Elevator(db.Model):
-    __tablename__ = 'elevators'
-    id = db.Column(db.Integer, primary_key=True)
-    building_id = db.Column(db.Integer, db.ForeignKey('buildings.id'), nullable=False)
-    serial_number = db.Column(db.Unicode(50), unique=True, nullable=False)
-    status = db.Column(db.Unicode(50), nullable=False)
-    install_date = db.Column(db.Date, nullable=False)
